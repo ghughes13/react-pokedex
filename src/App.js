0117a40b1b3ec71fa5d3eb292bfde01ae9b81f50
Garrett <guitarguy13@ymail.com>
2019-10-16 08:22:18 -0500

    Connected to API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+export default function App() {
+  
+  const getZip = () => {
+    console.log('hi');
+      axios
+        .get("https://pokeapi.co/api/v2/pokemon/1/")
+        .then(result => console.log(result));
+  }
+
+  getZip();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
-
-export default App;
